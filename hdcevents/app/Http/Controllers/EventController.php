cc72d35aa988e00ecaf9df5454aa8c1623f04370
samuca23 <samuelchiodini@hotmail.com>
2023-02-23 01:27:46 -0300

    Aula 30  - Removendo presença do evento

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -55,9 +55,21 @@ class EventController extends Controller
     public function show($id)
     {
         $oEvent = Event::findOrFail($id);
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if ($user) {
+            $userEvents = $user->eventsAsParticipant->toArray();
+            foreach($userEvents as $userEvent) {
+                if ($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
         $eventOwner = User::where('id', $oEvent->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $oEvent, 'eventOwener' => $eventOwner]);
+        return view('events.show', ['event' => $oEvent, 'eventOwener' => $eventOwner, 'hasuserjoined' => $hasUserJoined]);
     }
 
     public function dashboard()
@@ -110,9 +122,18 @@ class EventController extends Controller
     public function joinEvent($id)
     {
         $user = auth()->user();
-        $user->eventsAsParticipant->attach($id);
+        $user->eventsAsParticipant()->attach($id);
         $event = Event::findOrFail($id);
 
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
+
+    public function leaveEvent($id)
+    {
+        $user = auth()->user();
+        $user->eventsAsParticipant()->detach($id);
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu do evento ' . $event->title);
     }
 }
