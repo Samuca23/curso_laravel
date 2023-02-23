26d62360bdcb1e2697d82a2854e57e51c52088de
samuca23 <samuelchiodini@hotmail.com>
2023-02-23 00:59:38 -0300

    Aula 28 - Relação many to many

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -100,4 +100,12 @@ class EventController extends Controller
 
         return redirect('/dashboard')->with('msg', 'Evento alterado com sucesso!');
     }
+
+    public function joinEvent($id) {        
+        $user = auth()->user();
+        $user->eventsAsParticipant()->attach($id);
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
 }
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
-
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
 Route::post('/events', [EventController::class, 'store']);
 
 Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
